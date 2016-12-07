22196e9691aa083087b7c0a256afc5b3e4893e68
Casey Waldren <casey@nullspacevr.com>
2016-12-06 18:10:15 -0800

    Initial commit

---
+++ NSVRService/Interop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSVRService
+{
+	public static class Interop
+	{
+
+		[DllImport("NS_Unreal_SDK", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+		public static extern IntPtr NSEngine_Create();
+
+		[DllImport("NS_Unreal_SDK", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+		public static extern void NSEngine_Destroy(IntPtr ptr);
+
+		[DllImport("NS_Unreal_SDK", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+		public static extern void NSEngine_Update(IntPtr ptr);
+
+		[DllImport("NS_Unreal_SDK", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+		public static extern bool NSEngine_Shutdown(IntPtr ptr);
+
+	}
+}
---
+++ NSVRService/MyNewService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace NSVRService
+{
+	public partial class MyNewService : ServiceBase
+	{
+		private IntPtr _ptr;
+		private ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
+		private Thread _thread;
+		public MyNewService()
+		{
+			InitializeComponent();
+			if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+			{
+				System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
+			}
+
+			eventLog1.Source = "MySource";
+			eventLog1.Log = "MyNewLog";
+
+		}
+
+		internal void TestStartupAndStop(string[] args)
+		{
+			this.OnStart(args);
+			Console.ReadLine();
+			this.OnStop();
+		}
+		private void WorkerThreadFunc()
+		{
+			while (!_shutdownEvent.WaitOne(0))
+			{
+				Interop.NSEngine_Update(_ptr);
+			}
+		}
+		protected override void OnStart(string[] args)
+		{
+			//System.Diagnostics.Debugger.Launch();
+
+			eventLog1.WriteEntry("In OnStart 3.0");
+
+			_ptr = Interop.NSEngine_Create();
+			_thread = new Thread(WorkerThreadFunc);
+			_thread.Name = "NSVR Engine Thread";
+			_thread.IsBackground = true;
+			_thread.Start();
+
+		}
+
+		
+		protected override void OnStop()
+		{
+			_shutdownEvent.Set();
+			if (!_thread.Join(3000))
+			{
+				_thread.Abort();
+			}
+			eventLog1.WriteEntry("In OnStop!!");
+			Interop.NSEngine_Shutdown(_ptr);
+			Thread.Sleep(100);
+			Interop.NSEngine_Destroy(_ptr);
+		}
+	}
+}
