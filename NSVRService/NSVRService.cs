f688b198b6840d18188ceb575625138dbe50e29a
Casey Waldren <casey@nullspacevr.com>
2017-01-05 16:58:10 -0800

    Remove a bunch of dead code

--- NSVRService/NSVRService.cs
+++ NSVRService/NSVRService.cs
@@ -16,32 +16,13 @@ namespace NSVRService
 	public partial class NSVRService : ServiceBase
 	{
 		private IntPtr _ptr;
-		private NamedPipeServerStream _pipeServer;
-		private Thread _monitorThread;
 		private ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
 		public NSVRService()
 		{
 			InitializeComponent();
-		//	_pipeServer = new NamedPipeServerStream("testpipe", PipeDirection.Out, 1, //PipeTransmissionMode.Message, PipeOptions.Asynchronous);
-			
-			//_pipeServer.BeginWaitForConnection(HandlePipeConnection, null);
-		//	System.Diagnostics.Debugger.Launch();
+		
 		}
-/*
-		private void HandlePipeConnection(IAsyncResult ar)
-		{
-			//	_pipeServer.EndWaitForConnection(ar);
-			StreamWriter sw = new StreamWriter(_pipeServer);
-			
-				if (_pipeServer.CanWrite)
-				{
-					
-				System.Threading.Thread.Sleep(500);
-				sw.WriteLine("HELLO WORLD!");
 
-			}
-
-		}*/
 		internal void TestStartupAndStop(string[] args)
 		{
 			this.OnStart(args);
@@ -51,35 +32,13 @@ namespace NSVRService
 		
 		protected override void OnStart(string[] args)
 		{
-			//System.Diagnostics.Debugger.Launch();
-
-
+		
 			_ptr = Interop.NSEngine_Create();
 			Interop.NSEngine_StartThread(_ptr);
 
-		//	_monitorThread = new Thread(MonitorPipe);
-		//	_monitorThread.IsBackground = true;
-		//	_monitorThread.Name = "Monitor Thread";
-		//	_monitorThread.Start();
 		}
 		
-		/*private void MonitorPipe()
-		{
-			StreamWriter sw = new StreamWriter(_pipeServer);
-			sw.AutoFlush = true;
-			while (!_shutdownEvent.WaitOne(0))
-			{
-				try
-				{
-					sw.WriteLine("Hi");
-				}
-				catch(IOException e)
-				{
-					//couldn't write
-				}
-			}
-		}
-		*/
+	
 		protected override void OnStop()
 		{
 	
@@ -88,11 +47,7 @@ namespace NSVRService
 			Interop.NSEngine_Destroy(_ptr);
 
 			_shutdownEvent.Set();
-			//if (!_monitorThread.Join(300))
-			//{
-				//_monitorThread.Abort();
-			//}
-
+			
 		}
 	}
 }
