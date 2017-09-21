d554f97f5c901ed8d1c64ae82f2acb531d5034a8
Casey Waldren <casey@nullspacevr.com>
2017-09-21 12:04:20 -0700

    More naming updates

--- NSVRGui/Program.cs
+++ NSVRGui/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ServiceProcess;
 using System.Windows.Forms;
-
+using System.Linq;
 namespace NSVRGui
 {
 
@@ -29,48 +29,98 @@ namespace NSVRGui
 			return string.Format("{0}.{1}", Major, Minor);
 		}
 	}
+
 	public class MyCustomApplicationContext : ApplicationContext
 	{
+		/// <summary>
+		/// Used to periodically poll the plugin for the status of the service, and any new devices
+		/// </summary>
 		private Timer _checkStatusTimer;
-		private NotifyIcon trayIcon;
-		private ServiceController sc;
-		private IntPtr _plugin;
-		private Timer sendHapticDelayed;
-		private Dictionary<string, Interop.NSVR_DeviceInfo> devices;
-		private ServiceVersion serviceVersion; 
 
-		private MenuItem connectedDevices;
-		bool disposed = false;
+		/// <summary>
+		/// The little Hardlight icon in the tray. Can be changed to represent different states.
+		/// </summary>
+		private NotifyIcon _trayIcon;
 
-		IntPtr _testEffectData;
+		/// <summary>
+		/// Responsible for starting and stopping the Windows Service for the Hardlight Platform
+		/// </summary>
+		private ServiceController _serviceController;
+
+		/// <summary>
+		/// Raw pointer to our Hardlight.dll instance, used to interact with the system
+		/// </summary>
+		private IntPtr _pluginPtr;
+ 
+		/// <summary>
+		/// How long we delay before playing the test routine. This is needed because
+		/// the plugin takes a few moments to initialize under the hood, so a test routine submitted too quickly
+		/// will not run.
+		/// </summary>
+		private Timer _hapticsDelay;
+
+		/// <summary>
+		/// Our cached list of devices present in the system. Key is the device name.
+		/// </summary>
+		private Dictionary<string, Interop.NSVR_DeviceInfo> _cachedDevices;
+
+		/// <summary>
+		/// Cached service version information, used to display in the Version Info window.
+		/// </summary>
+		private ServiceVersion _cachedServiceVersion; 
+
+		/// <summary>
+		/// The little menu of devices when you right click on the tray icon
+		/// </summary>
+		private MenuItem _deviceMenuList;
+
+		/// <summary>
+		/// Needed for IDisposable support (because of the interop with native Hardlight.dll)
+		/// </summary>
+		private bool _disposed = false;
+
+		/// <summary>
+		/// Holds NSVR_Timeline used in the test routine
+		/// </summary>
+		private IntPtr _testRoutineTimeline;
+
 		override protected void Dispose(bool disposing)
 		{
-			if (disposed)
+			if (_disposed)
 			{
 				return;
 			}
 			if (disposing)
 			{
-				Interop.NSVR_Timeline_Release(ref _testEffectData);
-				Interop.NSVR_System_Release(ref _plugin);
+				Interop.NSVR_Timeline_Release(ref _testRoutineTimeline);
+				Interop.NSVR_System_Release(ref _pluginPtr);
 			}
-			disposed = true;
+			_disposed = true;
 			base.Dispose(disposing);
 		}
+
 		~MyCustomApplicationContext()
 		{
 			Dispose(false);
 		}
+
 		public MyCustomApplicationContext()
 		{
-			serviceVersion = new ServiceVersion();
-			devices = new Dictionary<string, Interop.NSVR_DeviceInfo>();
-			connectedDevices = new MenuItem("Devices", new MenuItem[] { });
 
-			if (Interop.NSVR_FAILURE(Interop.NSVR_System_Create(ref _plugin)))
+			_pluginPtr = IntPtr.Zero;
+
+			if (Interop.NSVR_FAILURE(Interop.NSVR_System_Create(ref _pluginPtr)))
 			{
-				//bad news
+				MessageBox.Show("Failed to create the Hardlight plugin! Application will now exit.", "Hardlight Service", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Exit();
 			}
+
+
+			_cachedServiceVersion = new ServiceVersion();
+			_cachedDevices = new Dictionary<string, Interop.NSVR_DeviceInfo>();
+			_deviceMenuList = new MenuItem("Devices", new MenuItem[] { });
+
+
 
 			Interop.AreaFlag[] order = {
 				Interop.AreaFlag.Forearm_Left,
@@ -94,7 +144,7 @@ namespace NSVRGui
 				Interop.AreaFlag.Forearm_Right
 
 			};
-			Interop.NSVR_Timeline_Create(ref _testEffectData);
+			Interop.NSVR_Timeline_Create(ref _testRoutineTimeline);
 			float offset = 0.0f;
 			//foreach (var flag in order)
 			//{
@@ -112,25 +162,25 @@ namespace NSVRGui
 			//	offset += 0.1f;
 			//}
 
-		
 
 
 
-			sc = new ServiceController();
-			sc.ServiceName = "NullSpace VR Runtime";
+
+			_serviceController = new ServiceController();
+			_serviceController.ServiceName = "Hardlight VR Runtime";
 
 			var myMenu = new ContextMenu(new MenuItem[] {
 					new MenuItem("Enable Runtime", StartService),
 					new MenuItem("Disable Runtime", StopService),
 					new MenuItem("Test Suit", TestSuit),
 					new MenuItem("Version info", VersionInfo),
-					connectedDevices,
-				new MenuItem("Exit", Exit)
-				});
+					_deviceMenuList,
+				new MenuItem("Exit", new EventHandler((object o, EventArgs e) => { Exit(); }))
+			});
 
 		
 			// Initialize Tray Icon
-			trayIcon = new NotifyIcon()
+			_trayIcon = new NotifyIcon()
 			{
 				Icon = Properties.Resources.TrayIconServiceOff,
 				ContextMenu = myMenu,
@@ -144,179 +194,183 @@ namespace NSVRGui
 
 			
 			StartService(null, null);
-			sendHapticDelayed = new Timer();
-			sendHapticDelayed.Interval = 500;
-			sendHapticDelayed.Tick += DelayWhilePluginInitializes_Tick;
+			_hapticsDelay = new Timer();
+			_hapticsDelay.Interval = 500;
+			_hapticsDelay.Tick += DelayWhilePluginInitializes_Tick;
 
+		}
+		private bool isServiceActuallyConnected()
+		{
+			Interop.NSVR_ServiceInfo serviceInfo = new Interop.NSVR_ServiceInfo();
+
+			if (Interop.NSVR_SUCCESS(Interop.NSVR_System_GetServiceInfo(_pluginPtr, ref serviceInfo)))
+			{
+				_cachedServiceVersion.Major = serviceInfo.ServiceMajor;
+				_cachedServiceVersion.Minor = serviceInfo.ServiceMinor;
+				return true;
+			}
+
+			return false;
+		}
+
+		private Dictionary<string, Interop.NSVR_DeviceInfo> fetchKnownDevices()
+		{
+			Interop.NSVR_DeviceInfo_Iter iter = new Interop.NSVR_DeviceInfo_Iter();
+			Interop.NSVR_DeviceInfo_Iter_Init(ref iter);
+
+			var newDevices = new Dictionary<string, Interop.NSVR_DeviceInfo>();
+			while (Interop.NSVR_DeviceInfo_Iter_Next(ref iter, _pluginPtr))
+			{
+				newDevices.Add(new string(iter.DeviceInfo.Name), iter.DeviceInfo);
+			}
+
+			return newDevices;
+		}
+
+		private void removeUnrecognizedDevicesFromMenu(Dictionary<string, Interop.NSVR_DeviceInfo> newDevices)
+		{
+			foreach (var device in _cachedDevices)
+			{
+				if (!newDevices.ContainsKey(device.Key))
+				{
+					_deviceMenuList.MenuItems.RemoveByKey(device.Key);
+				}
+			}
+		}
+
+		private void addRecognizedDevicesToMenu(Dictionary<string, Interop.NSVR_DeviceInfo> newDevices)
+		{
+			foreach (var device in newDevices)
+			{
+				if (!_cachedDevices.ContainsKey(device.Key))
+				{
+					MenuItem a = new MenuItem(device.Key);
+					a.Name = device.Key;
+					_deviceMenuList.MenuItems.Add(a);
+				}
+			}
 		}
 		private void CheckStatusSuit(object sender, EventArgs args)
 		{
-
-			sc.Refresh();
+			_serviceController.Refresh();
 
 			try
 			{
-				var serviceStatus = sc.Status;
-				if (serviceStatus == ServiceControllerStatus.Running)
+				var serviceStatus = _serviceController.Status;
+
+				if (serviceStatus != ServiceControllerStatus.Running)
 				{
-					Interop.NSVR_ServiceInfo serviceInfo = new Interop.NSVR_ServiceInfo();
-
-					
-					if (Interop.NSVR_SUCCESS(Interop.NSVR_System_GetServiceInfo(_plugin, ref serviceInfo))) {
-						this.serviceVersion.Major = serviceInfo.ServiceMajor;
-						this.serviceVersion.Minor = serviceInfo.ServiceMinor;
-					}
-
-					Interop.NSVR_DeviceInfo_Iter iter = new Interop.NSVR_DeviceInfo_Iter();
-					Interop.NSVR_DeviceInfo_Iter_Init(ref iter);
-
-					bool anythingPresent = false;
-
-					var newDevices = new Dictionary<string, Interop.NSVR_DeviceInfo>();
-					while (Interop.NSVR_DeviceInfo_Iter_Next(ref iter, _plugin))
-					{
-
-						newDevices.Add(new string(iter.DeviceInfo.Name), iter.DeviceInfo);
-						anythingPresent = true;
-					}
-
-					foreach (var device in devices)
-					{
-						if (!newDevices.ContainsKey(device.Key))
-						{
-							connectedDevices.MenuItems.RemoveByKey(device.Key);
-						}
-
-					}
-					foreach (var device in newDevices)
-					{
-						if (!devices.ContainsKey(device.Key))
-						{
-							MenuItem a = new MenuItem(device.Key);
-							a.Name = device.Key;
-							connectedDevices.MenuItems.Add(a);
-						}
-					}
-					devices = newDevices;
-					if (anythingPresent) {
-
-
-						trayIcon.Icon = Properties.Resources.TrayIconServiceOnSuitConnected;
-
-					}
-					else
-					{
-						trayIcon.Icon = Properties.Resources.TrayIconServiceOn;
-
-					}
+					_trayIcon.Icon = Properties.Resources.TrayIconServiceOff;
+					return;
 				}
-				else
+
+				if (isServiceActuallyConnected())
 				{
-					trayIcon.Icon = Properties.Resources.TrayIconServiceOff;
+					var newDevices = fetchKnownDevices();
+					removeUnrecognizedDevicesFromMenu(newDevices);
+					addRecognizedDevicesToMenu(newDevices);
+
+					_cachedDevices = newDevices;
+
+					bool anythingConnected = _cachedDevices.Any(kvp => kvp.Value.Status == Interop.NSVR_DeviceStatus.Connected);
+
+					_trayIcon.Icon = anythingConnected? 
+							Properties.Resources.TrayIconServiceOnSuitConnected :
+							Properties.Resources.TrayIconServiceOn;
 				}
 			}
 			catch (System.InvalidOperationException e)
 			{
-				//loooks like the service was uninstalled, so we should quit the app!
-				trayIcon.Visible = false;
-				Application.Exit();
+				//Perhaps the service was uninstalled..?
+				_trayIcon.Visible = false;
+				Exit();
 			}
-
-
 		}
 
 		void StartService(object sender, EventArgs e)
 		{
 	
-			if (sc.Status == ServiceControllerStatus.Stopped)
+			if (_serviceController.Status == ServiceControllerStatus.Stopped)
 			{
 				try
 				{
-					sc.Start();
-					sc.WaitForStatus(ServiceControllerStatus.Running, new TimeSpan(0, 0, 5));
-					trayIcon.Icon = Properties.Resources.TrayIconServiceOn;
+					_serviceController.Start();
+					_serviceController.WaitForStatus(ServiceControllerStatus.Running, new TimeSpan(0, 0, 5));
+					_trayIcon.Icon = Properties.Resources.TrayIconServiceOn;
 
 				}
 				catch (InvalidOperationException)
 				{
-					MessageBox.Show("Could not start the NullSpace Runtime!");
+					MessageBox.Show("Could not start the Hardlight Runtime!");
 				} 
 				catch (System.ServiceProcess.TimeoutException)
 				{
-					MessageBox.Show("Took too long to start the NullSpace Runtime! Runtime is stopped.");
+					MessageBox.Show("Took too long to start the Hardlight Runtime! Runtime is stopped.");
 				}
 			}
 		}
 
 		void StopService(object sender, EventArgs e)
 		{
-			if (sc.Status == ServiceControllerStatus.Running)
+			if (_serviceController.Status == ServiceControllerStatus.Running)
 			{
 				try
 				{
-					sc.Stop();
-					sc.WaitForStatus(ServiceControllerStatus.Stopped, new TimeSpan(0,0,5));
-					trayIcon.Icon = Properties.Resources.TrayIconServiceOff;
+					_serviceController.Stop();
+					_serviceController.WaitForStatus(ServiceControllerStatus.Stopped, new TimeSpan(0,0,5));
+					_trayIcon.Icon = Properties.Resources.TrayIconServiceOff;
 				}
 				catch (InvalidOperationException)
 				{
-					MessageBox.Show("Could not stop the NullSpace Runtime!");
+					MessageBox.Show("Could not stop the Hardlight Runtime!");
 				}
 				catch (System.ServiceProcess.TimeoutException)
 				{
-					MessageBox.Show("Took too long to stop the NullSpace Runtime! ");
+					MessageBox.Show("Took too long to stop the Hardlight Runtime! ");
 				}
 			}
 		}
 
 		void TestSuit(object sender, EventArgs e)
 		{
-			if (sc.Status != ServiceControllerStatus.Running)
+			if (_serviceController.Status != ServiceControllerStatus.Running)
 			{
 				StartService(null, null);
-				
-				sendHapticDelayed.Start();
+				_hapticsDelay.Start();
 			} else
 			{
 				CreateAndPlayHaptic();
 			}
-			
-
-
 		}
+
 		private void CreateAndPlayHaptic()
 		{
 			IntPtr playbackHandle = IntPtr.Zero;
 			Interop.NSVR_PlaybackHandle_Create(ref playbackHandle);
 
-			Interop.NSVR_Timeline_Transmit( _testEffectData, _plugin, playbackHandle);
+			Interop.NSVR_Timeline_Transmit( _testRoutineTimeline, _pluginPtr, playbackHandle);
 			Interop.NSVR_PlaybackHandle_Command(playbackHandle, Interop.NSVR_PlaybackCommand.Play);
 			Interop.NSVR_PlaybackHandle_Release(ref playbackHandle);
 
 		}
+
 		private void DelayWhilePluginInitializes_Tick(object sender, EventArgs e)
 		{
 			CreateAndPlayHaptic();
-			sendHapticDelayed.Stop();
+			_hapticsDelay.Stop();
 		}
 
-	
-
-		void VersionInfo(object sender, EventArgs e)
+		private void VersionInfo(object sender, EventArgs e)
 		{
-			
-			VersionInfo v = new VersionInfo(this.serviceVersion);
-
+			VersionInfo v = new VersionInfo(this._cachedServiceVersion);
 			v.Show();
-			
-
 		}
-		void Exit(object sender, EventArgs e)
+		private void Exit()
 		{
 			_checkStatusTimer.Stop();
 			
 			// Hide tray icon, otherwise it will remain shown until user mouses over it
-			trayIcon.Visible = false;
+			_trayIcon.Visible = false;
 			StopService(null, null);
 			Application.Exit();
 		}
